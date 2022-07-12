410acfa7cd0207545e7b7f6f33b9c8b237e56d1d
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 22:56:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    koi.koi_colorcali("red")
+    flag = !(flag)
+})
+koi.koi_onlinetrack(function (x1, y1, x2, y2) {
 	
+})
+let flag = false
+koi.koi_init_pw(koi.SerialPorts.PORT2)
+flag = false
+basic.forever(function () {
+    if (flag) {
+        koi.koi_track_line("red")
+        basic.pause(500)
+    }
 })
