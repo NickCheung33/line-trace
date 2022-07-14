0cfd021fb192abf5e936ae3d63a1fb9c91e85e98
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-14 23:49:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,33 @@
-input.onButtonPressed(Button.A, function () {
-    koi.koi_colorcali("red")
-    flag = !(flag)
+MakerCloud_KOI.registerTopicMessageHandler("ZBOP3RO2", function (receivedMessage) {
+    if (receivedMessage == "trace") {
+        koi.koi_colorcali("black")
+        flag = !(flag)
+    } else if (receivedMessage == "stop") {
+        receivedMessage = flag
+    }
 })
 koi.koi_onlinetrack(function (x1, y1, x2, y2) {
-	
+    if (x1 < x2 + 1) {
+        powerbrick.MotorRun(powerbrick.Motors.M1, 100)
+        powerbrick.MotorRun(powerbrick.Motors.M2, 255)
+    } else if (x2 < x1 + 1) {
+        powerbrick.MotorRun(powerbrick.Motors.M1, 255)
+        powerbrick.MotorRun(powerbrick.Motors.M2, 100)
+    } else if (x2 == x1) {
+        powerbrick.MotorRun(powerbrick.Motors.M1, 255)
+        powerbrick.MotorRun(powerbrick.Motors.M2, 255)
+    }
 })
-let flag = false
+let flag = ""
 koi.koi_init_pw(koi.SerialPorts.PORT2)
+koi.koi_lcd_direction(koi.LcdDirection.Front)
+koi.koi_join_ap("ssid", "password")
 flag = false
+MakerCloud_KOI.connectMakerCloudMQTT()
+MakerCloud_KOI.subscribeTopic("ZBOP3RO2")
 basic.forever(function () {
     if (flag) {
-        koi.koi_track_line("red")
+        koi.koi_track_line("black")
         basic.pause(500)
     }
 })
